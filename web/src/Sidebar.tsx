f72aedd759321dd0cea2dc03bb2992b23147ec4f
Han Yu <han@windmill.engineering>
2019-05-16 15:55:21 -0400

    web: add consistent badges for tab and sidebar

--- web/src/Sidebar.tsx
+++ web/src/Sidebar.tsx
@@ -139,7 +139,9 @@ class Sidebar extends PureComponent<SidebarProps> {
             </span>
             <span className="resLink-name">{item.name}</span>
             {item.numberOfAlerts() > 0 ? (
-              <span className="resLink-alertNum">{item.numberOfAlerts()}</span>
+              <span className="resLink-alertBadge">
+                {item.numberOfAlerts()}
+              </span>
             ) : (
               ""
             )}
